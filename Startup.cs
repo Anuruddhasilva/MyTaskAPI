39c10324fc8d8658ec9ce43cc109b03e9a3c00c1
Anuruddha <contactanuruddha@gamil.com>
2021-10-01 00:15:52 +0200

    test

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace MyTaskAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
