3e24441f9d739b123227bf2ca3a93409a066c28e
Anuruddha <contactanuruddha@gamil.com>
2021-10-01 01:12:09 +0200

    Create and sepatated odels and services

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyTaskAPI.Models;
+using MyTaskAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private TodoService _todoservice;
+
+        public ToDosController()
+        {
+            _todoservice = new TodoService();
+        }
+
+        [HttpGet("(Id)")]
+        public IActionResult GetTodos(int Id)
+        {
+            var getAllTodos = _todoservice.GetAllTodos().Where(t => t.Id==Id);
+            return Ok(getAllTodos);
+        }
+
+
+    
+    }
+}
